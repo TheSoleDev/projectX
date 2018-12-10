08f7ba2798b93867864c96f736ae410f867eaeae
Julius Garcia <garciajuliusb@yahoo.com>
2018-12-10 18:10:23 +0800

    Iniital Commit  -Boilerplate

---
+++ offline.js
@@ -0,0 +1,1 @@
+{"version":1544434985917,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/bg-sheet0.png","images/bg_sea-sheet0.png","icons/icon-256.png","icons/loading-logo.png","icons/icon-16.png","icons/icon-32.png","icons/icon-64.png","icons/icon-128.png","icons/icon-512.png","register-sw.js"]}
